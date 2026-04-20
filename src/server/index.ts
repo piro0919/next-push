980311744b1aee1ce84d0f18bb45a6e2c46ed3e9
piro0919 <piro.haniwa@gmail.com>
2026-04-21 01:21:38 +0900

    feat(server): wire up public server-side entry point

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,16 @@
-export {};
+export type {
+  PushPayload,
+  PushSubscriptionJSON,
+  SendOptions,
+  SendResult,
+  Urgency,
+  VAPIDKeys,
+} from "../core/types";
+export type {
+  CreatePushHandlerConfig,
+  PushHandler,
+} from "./createPushHandler";
+export { createPushHandler } from "./createPushHandler";
+export type { SendPushConfig } from "./sendPush";
+export { sendPush } from "./sendPush";
+export { generateVAPIDKeys } from "./vapid/keys";
