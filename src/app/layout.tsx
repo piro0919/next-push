4613579525aafd0255aaf1e130ac368b65ea4591
piro0919 <piro.haniwa@gmail.com>
2026-08-17 04:01:59 +0900

    feat(seo): add a social preview image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://next-push.kkweb.io"),
   applicationName: "next-push",
   appleWebApp: {
     capable: true,
